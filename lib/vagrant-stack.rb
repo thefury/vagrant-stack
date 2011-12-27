ceb1250547a1ec327dc0857dfc4bae90e8ea0b34
Trevor Oke <trevor@trevoroke.com>
2011-12-27 13:36:07 -0500

    initial commit

---
+++ lib/vagrant-stack.rb
@@ -0,0 +1,7 @@
+require "vagrant-stack/version"
+
+module Vagrant
+  module Stack
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant-stack/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module Stack
+    VERSION = "0.0.1"
+  end
+end
